0dc7afea71b5382975ad8fdd12d759754d497812
Alex Magalhaes <alex@pensato.com.br>
2018-02-24 20:29:24 -0300

    CORS configured.

--- src/main/java/net/pensato/udemy.beeper/security/WebSecurity.kt
+++ src/main/java/net/pensato/udemy.beeper/security/WebSecurity.kt
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
-
 @EnableWebSecurity
 open class WebSecurity(
         val userDetailsService: UserDetailsService,
@@ -44,8 +43,24 @@ open class WebSecurity(
 
     @Bean
     open fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.allowCredentials = true
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.allowedHeaders = listOf("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "Cache-Control", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+        configuration.maxAge = 3600
+        configuration.exposedHeaders = listOf("Authorization")
+
         val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", CorsConfiguration().applyPermitDefaultValues())
+        source.registerCorsConfiguration("/**", configuration)
+
         return source
     }
+
+
 }
